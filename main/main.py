df9f57cd97124859dc487081cbdcd8cee5f52c3d
Fulanodtals <fulanodtalsofc@gmail.com>
2024-07-24 11:15:37 -0300

    showing ip adress

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 import sys
 import socket
+import threading
 import os
 from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QVBoxLayout, QListWidget, QGridLayout
 from PyQt6.QtGui import QIntValidator
@@ -21,28 +22,26 @@ class MainApp(QWidget):
         self.label = {}
         self.userInput = {}
 
-        self.label['ip'] = QLabel('ip') 
-        self.label['port'] = QLabel('port') 
-        self.userInput['ip'] = QLineEdit('0.0.0.0')
+        self.label['ip'] = QLabel('your ip') 
+        self.label['port'] = QLabel('select port') 
+        self.label['myip'] = QLabel()
         self.userInput['port'] = QLineEdit('12345')
-        
-
         self.list = QListWidget()
 
         
         layout.addWidget(title,                  0,0,1,2)
         layout.addWidget(self.label['ip'],       1,0,1,1)
         layout.addWidget(self.label['port'],     1,1,1,1)
-        layout.addWidget(self.userInput['ip'],   2,0,1,1)
+        layout.addWidget(self.label['myip'],     2,0,1,1)
         layout.addWidget(self.userInput['port'], 2,1,1,1)
         layout.addWidget(self.list,              3,0,1,2)
         
-
-        self.conectCF
+        
+        threading.Thread(target=self.conectCF, daemon=True).start()
+        threading.Thread(target=self.getIpAdress, daemon=True).start()
         
     def conectCF(self):
-        ip = self.userInput['ip'].text()
-        port = self.userInput['port'].text()
+        port = int(self.userInput['port'].text())
         
         print('iniciando server')
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,12 +53,24 @@ class MainApp(QWidget):
             running, addr = server.accept()
             mensage = running.recv(1024).decode('utf-8')
 
-            print(f'decodificando: {mensage}')
-            
             if mensage == 'desligar':
                 print('desligando')
 
             running.close()
+
+
+
+    def getIpAdress(self):
+        try:
+            getip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            getip.connect(("8.8.8.8", 80))  # O IP 8.8.8.8 é do Google DNS, qualquer IP externo serve
+            ip = getip.getsockname()[0]
+            getip.close()
+            self.label['myip'].setText(f'{ip}')
+            self.label['myip'].adjustSize()
+        except:
+            self.label['myip'].setText('ip nao encontrado')
+            self.label['myip'].adjustSize()
 
 
 if __name__ == "__main__":
